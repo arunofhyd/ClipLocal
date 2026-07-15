fda5fbc7162582995182b95b8a1bab19e9a44db3
arunofhyd <104165035+arunofhyd@users.noreply.github.com>
2026-07-15 14:35:00 +0530

    Update main.swift

--- main.swift
+++ main.swift
@@ -302,7 +302,7 @@ struct ContentView: View {
                             }
                             .buttonStyle(PlainButtonStyle())
                         }
-                        .padding(.vertical, 0)
+                        .padding(.vertical, 4)
                         .padding(.horizontal, 4)
                         .contentShape(RoundedRectangle(cornerRadius: 8))
                         .onTapGesture(count: 2) {
@@ -344,7 +344,7 @@ struct ContentView: View {
                         .opacity(0)
                         .frame(width: 0, height: 0)
                     }
-                    .listRowInsets(EdgeInsets(top: -4, leading: 12, bottom: -4, trailing: 12))
+                    .listRowInsets(EdgeInsets(top: 4, leading: 12, bottom: 4, trailing: 12))
                     .listRowSeparator(.visible)
                 }
             }
