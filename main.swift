202dbdbfacc33ff3a6627ffb63a29d1f31e0989a
Arun Thomas <arunthomas04042001@gmail.com>
2026-07-17 18:36:54 +0530

    style: Anchor listRowSeparator to prevent movement on leading swipe

--- main.swift
+++ main.swift
@@ -430,6 +430,7 @@ struct ContentView: View {
         }
         .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
         .listRowSeparator(.visible)
+        .alignmentGuide(.listRowSeparatorLeading) { _ in 0 }
     }
 
     // MARK: - Helpers
